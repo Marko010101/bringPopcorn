828f53df9fed51991773c62dd1673b80ff9cded2
Marko010101 <baghashvili.marko@gmail.com>
2024-01-08 23:58:10 +0400

    Created a reusable component and passed it a few props to be changeable.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating.js";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars </p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={30} color="black" className="test" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
