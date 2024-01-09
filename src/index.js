dbc835ca6fd27ce61abc03cc8f3c26ed06071744
Marko010101 <baghashvili.marko@gmail.com>
2024-01-09 22:57:21 +0400

    Created new expanded text

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating.js";
+import App from "./ShowMoreText.js";
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -18,13 +19,14 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
+    {/* <App />
     <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={30} color="black" className="test" defaultRating={3} />
 
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
